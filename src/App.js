64396ebef9b172fb1c44b30f05f840134411b0bd
gdias <gdias@Infrastructures-MacBook-Pro-2.local>
2020-05-24 15:30:00 -0300

    alterações desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    const loadRepositories = async () => {
+      const response = await api.get("/repositories");
+
+      setRepositories(response.data);
+    };
+
+    loadRepositories();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const newRepository = { title: `teste-${new Date().getMilliseconds()}` };
+
+    const response = await api.post("/repositories", newRepository);
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    const filteredRepos = repositories.filter((repo) => repo.id !== id);
+
+    setRepositories(filteredRepos);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repo) => (
+          <li>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
